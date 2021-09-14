c36a44e169ce3a6a0bc365617154d2ec54d01b1f
Sergey <surzhiks.85@mail.ru>
2021-09-15 01:28:41 +0300

    glitch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('hello')
+let menuText = document.querySelectorAll('.menu_text');
+
+function addClassGlitch(){
+	setInterval(function(){
+		let randEl = menuText[Math.floor(Math.random()*menuText.length)]
+		randEl.classList.add('glitch')
+		setTimeout(function(){
+			randEl.classList.remove('glitch')
+		},500)
+	},7000)
+}
+addClassGlitch()
